bb07c5f14435a6f8f12e1db149168c2502778514
Julia Tseng <juliatseng12345@gmail.com>
2024-06-29 12:41:25 -0400

    Add home page

---
+++ app/components/About.jsx
@@ -0,0 +1,27 @@
+
+export default function About() {
+
+    return (
+        <div className="w-full space-y-8">
+            <div className="space-y-3">
+                <h1 className="font-semibold text-xl">now</h1>
+                    <p className="indent-7">independent research in interface design for AI products</p>
+                    <p className="indent-7">building design skills and exploring new tech from a product perspective</p>
+                    <p className="indent-7"><a className="underline decoration-solid text-violet-800" href="https://jteesang.substack.com/">writing</a> about ambition, community-building, and life reflections</p>
+            </div>
+            <div className="space-y-3">
+                <h1 className="font-semibold text-xl">past</h1>
+                    <p className="indent-7"><a className="underline decoration-solid text-violet-800" href="https://buildspace.so/">buildspace</a> - nights and weekends s5 <span className="text-gray-500"> - summer 2024</span></p>
+                    <p className="indent-14 italic">built an apple notes app powered by llm</p>
+                    <p className="indent-7">software engineer at <a className="underline decoration-solid text-violet-800" href="https://www.capitalone.com/">capital one</a><span className="text-gray-500"> - 2022-2024</span></p>
+                    <p className="indent-14 italic">probably sent you too many credit card upgrade offers</p>
+                    <p className="indent-7">bachelors degree in cs from UT Dallas <span className="text-gray-500"> - 2018-2021</span></p>
+
+            </div>
+
+            
+            
+            <h1 className="font-semibold text-xl">projects</h1>
+        </div>
+    )
+}
